90a03ee4f422fc5d10eb599017f5b86d95b84c77
Erik <glebellier@protonmail.com>
2021-10-08 08:36:21 +0200

    Restructure the package into src/sdia_python, update imports accordingly

--- tests/lab2/test_box_window.py
+++ tests/lab2/test_box_window.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from lab2.box_window import BoxWindow, UnitBoxWindow, BallWindow
+from sdia_python.lab2.box_window import BoxWindow, UnitBoxWindow, BallWindow
 
 
 def test_raise_type_error_when_something_is_called():
