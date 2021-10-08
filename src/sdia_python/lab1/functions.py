9897c1180c628678e5ca1778b002f1414aa3fc38
Erik <glebellier@protonmail.com>
2021-10-08 09:23:49 +0200

    Minor changes on previous functions

--- src/sdia_python/lab1/functions.py
+++ src/sdia_python/lab1/functions.py
@@ -1,29 +1,22 @@
 def is_unique(x):
-    """[This functions tell if all integer element are unique in the list]
+    return len(x) == len(set(x))
+
+
+def triangle_shape(n):
+    """Do triangle shape in string
 
     Args:
-        x ([int list]): [The input is a list of integer]
+        n (int): [description]
 
     Returns:
-        [Boolean]: [We return True if all element of the list are unique and False otherwise]
+        [type]: [description]
     """
-    for i in x:
-        if x.count(i) > 1 :
-            return False
-    return True
 
-def triangle_shape(height):
-    """[This is a funny little functions that creates triangle shaped of "x"]
-
-    Args:
-        height ([int]): [We enter the number of floor that the pyramid will have]]
-
-    Returns:
-        [list of str]: [We have a list of string that contains each floor of the pyramid]
-    """
-    print(f"height={height}")
+    base_n = 2 * n - 1
     res = []
-    for i in range(0,height):
-        pyr = "x"
-        res += [((height-i-1)*" "+(2*i+1)*pyr+(height-i-1)*" ")]
-    return res
+    for i in range(1, n + 1):
+        step_n = 2 * i - 1
+        res.append(" " * ((base_n - step_n) // 2) + "x" * step_n + " " *
+                   ((base_n - step_n) // 2))
+
+    return "\n".join(res)
