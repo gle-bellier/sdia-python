338c49c67551a8302d3a73b2a5773fbb1331873a
Erik <glebellier@protonmail.com>
2021-10-08 08:54:36 +0200

    Refactor code

---
+++ src/sdia_python/lab2/ball_window.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.special import gamma
+
+from sdia_python.lab2.utils import get_random_number_generator
+
+
+class BallWindow:
+    def __init__(self, center, radius):
+        """Create new ball window.
+
+        Args:
+            center (float array): coordinates of the center of the ball window.
+            radius (float): radius of the ball window.
+        """
+        self.center = center
+        self.radius = radius
+
+    def dist_to_center(self, x):
+        """Compute the distance of a given point to the center of the ball window.
+
+        Args:
+            x (float array): coordinates of the point of interest.
+
+        Returns:
+            float: distance of the given point to the center of the ball window.
+        """
+        # * Nice vectorization using numpy
+        # ? how about np.linalg.norm
+        return np.linalg.norm(x - self.center)
+
+    def __contains__(self, x):
+        """Compute whether a given point is contained in the ball window or not.
+
+        Args:
+            x (float array): coordinates of the point of interest.
+
+        Returns:
+            bool: point is in ball window or not.
+        """
+
+        # we need to compute the distance of the point of interest to the center of the ball window
+        return self.dist_to_center(x) < self.radius
+
+    def __str__(self):
+        """BallWindow: (center, radius)
+
+        Returns:
+            str: string representation of the ball window.
+        """
+        # * nice use of f-strings
+        return f"BallWindow (center : {self.center}, radius : {self.radius})"
+
+    def __len__(self):
+        """Indicates the number of dimension of the ball window.
+
+        Returns:
+            int: number of dimension of the ball window.
+        """
+        return len(self.center[0])
+
+    def indicator_function(self, args):
+        """Returns if args is in the ball window.
+
+    Args:
+        args (float array): The ball window  [description]
+
+    Returns:
+        bool: True if point in ball window else False.
+    """
+        return np.array([point in self for point in args])
+
+    # todo test it
+    def volume(self):
+        """Computes the volume of the ball.
+
+        Returns:
+            float: volume of the ball window.
+        """
+        # Using the gamma function to compute the volume of the ball
+        return np.power(np.pi,
+                        len(self) / 2) * np.power(
+                            self.radius, len(self)) / gamma(len(self) / 2 + 1)
--- tests/lab2/test_box_window.py
+++ tests/lab2/test_box_window.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
 
-from sdia_python.lab2.box_window import BoxWindow, UnitBoxWindow, BallWindow
+from sdia_python.lab2.box_window import BoxWindow, UnitBoxWindow
+from sdia_python.lab2.ball_window import BallWindow
 
 
 def test_raise_type_error_when_something_is_called():
@@ -14,8 +15,7 @@ def test_raise_type_error_when_something_is_called():
     "bounds, expected",
     [
         (np.array([[2.5, 2.5]]), "BoxWindow: [2.5, 2.5]"),
-        (np.array([[0, 5], [0, 5]]), "BoxWindow: [0, 5] x [0, 5]"
-         ),
+        (np.array([[0, 5], [0, 5]]), "BoxWindow: [0, 5] x [0, 5]"),
         (
             np.array([[0, 5], [-1.45, 3.14], [-10, 10]]),
             "BoxWindow: [0.0, 5.0] x [-1.45, 3.14] x [-10.0, 10.0]",
@@ -103,8 +103,7 @@ def test_in_ball_window(args, expected):
     "bounds, expected",
     [
         (np.array([[2.5, 2.5]]), 1),
-        (np.array([[0, 5], [0, 5]]), 2
-         ),
+        (np.array([[0, 5], [0, 5]]), 2),
         (
             np.array([[0, 5], [-1.45, 3.14], [-10, 10]]),
             3,
@@ -115,13 +114,11 @@ def test__len__(bounds, expected):
     assert (BoxWindow(bounds).__len__()) == expected
 
 
-
 @pytest.mark.parametrize(
     "bounds, expected",
     [
         (np.array([[2.5, 2.5]]), 1),
-        (np.array([[0, 5], [0, 5]]), 2
-         ),
+        (np.array([[0, 5], [0, 5]]), 2),
         (
             np.array([[0, 5], [-1.45, 3.14], [-10, 10]]),
             3,
@@ -132,60 +129,45 @@ def test_dimension(bounds, expected):
     assert (BoxWindow(bounds).dimension()) == expected
 
 
-
 @pytest.mark.parametrize(
     "bounds, expected",
     [
         (np.array([[2.5, 2.5]]), 0),
-        (np.array([[0, 5], [0, 5]]), 25
-         ),
+        (np.array([[0, 5], [0, 5]]), 25),
         (
             np.array([[0, 5], [-1.45, 3.14], [-10, 10]]),
             459,
         ),
     ],
 )
-
 def test_volume(bounds, expected):
     assert (BoxWindow(bounds).volume() == expected)
 
 
-
 @pytest.mark.parametrize(
     "bounds, n, expected",
     [
-        (np.array([[2.5, 2.5]]),2 , True),
-        (np.array([[0, 5], [0, 5]]), 4, True
-        ),
-        (
-            np.array([[0, 5], [-1.45, 3.14], [-10, 10]]), 8,
-            True
-        ),
+        (np.array([[2.5, 2.5]]), 2, True),
+        (np.array([[0, 5], [0, 5]]), 4, True),
+        (np.array([[0, 5], [-1.45, 3.14], [-10, 10]]), 8, True),
     ],
 )
-
 def test_rand_in_bounds(bounds, n, expected):
     a = BoxWindow(bounds)
-    assert (all(elm in a for elm in a.rand(n, rng = 1)) == expected)
+    assert (all(elm in a for elm in a.rand(n, rng=1)) == expected)
 
 
 @pytest.mark.parametrize(
     "bounds, n, expected",
     [
-        (np.array([[2.5, 2.5]]), 2 , np.array([[0.05910812],
-       [2.25231848]])),
-        (np.array([[0, 5], [0, 5]]), 4, np.array([[2.55910812, 4.75231848],
-       [0.72079806, 4.74324724],
-       [1.55915726, 2.11663224],
-       [4.13851297, 2.04599568]])
-        ),
-        (
-            np.array([[0, 5], [-1.45, 3.14], [-10, 10]]), 1,
-            np.array([[ 2.55910812,  2.91262837, -7.11680775]])
-        ),
+        (np.array([[2.5, 2.5]]), 2, np.array([[0.05910812], [2.25231848]])),
+        (np.array([[0, 5], [0, 5]]), 4,
+         np.array([[2.55910812, 4.75231848], [0.72079806, 4.74324724],
+                   [1.55915726, 2.11663224], [4.13851297, 2.04599568]])),
+        (np.array([[0, 5], [-1.45, 3.14], [-10, 10]
+                   ]), 1, np.array([[2.55910812, 2.91262837, -7.11680775]])),
     ],
 )
-
-def test_rand_number(bounds, n , expected):
+def test_rand_number(bounds, n, expected):
     a = BoxWindow(bounds)
-    assert (a.rand(n, rng = 1) == expected).all()
+    assert (a.rand(n, rng=1) == expected).all()
